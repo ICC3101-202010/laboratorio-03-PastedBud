56ef78ed8aa46b98f4f3fe002168d26ab0c0b3cf
Carlo Vitali <Carlo@Air-de-Carlos.iptv.microsoft.com>
2020-04-06 17:28:26 -0400

    primer repositorio

---
+++ Lab3CarloVitali/Lab3CarloVitali/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab3CarloVitali
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
